7d0be1a597e0208fea89899efaddf3385660638c
David_Scheers <MW^ol9sAdWxNgFK*f5Z36IK*J>
2019-05-06 15:15:27 +0200

    spring init

---
+++ src/main/java/be/ordina/wsconfig/WsConfigApplication.java
@@ -0,0 +1,13 @@
+package be.ordina.wsconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsConfigApplication.class, args);
+	}
+
+}
